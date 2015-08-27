f2d2a2c6bf9d003d6b5e0fa88561d1a7a0677652
Grant Paul <git@grantpaul.com>
2015-08-26 20:52:04 -0700

    Improve unknown file type computation, use more available checks.

--- Sources/pbxbuild/FileTypeResolver.cpp
+++ Sources/pbxbuild/FileTypeResolver.cpp
@@ -22,8 +22,8 @@ FileTypeResolver::
 std::unique_ptr<FileTypeResolver> FileTypeResolver::
 Resolve(pbxspec::Manager::shared_ptr const &specManager, std::string const &filePath)
 {
-    bool isFolder = FSUtil::TestForDirectory(filePath);
     bool isReadable = FSUtil::TestForRead(filePath);
+    bool isFolder = isReadable && FSUtil::TestForDirectory(filePath);
 
     std::string fileExtension = FSUtil::GetFileExtension(filePath);
     std::string fileName = FSUtil::GetBaseName(filePath);
@@ -32,17 +32,77 @@ Resolve(pbxspec::Manager::shared_ptr const &specManager, std::string const &file
     std::vector<uint8_t> fileContents;
 
     for (pbxspec::PBX::FileType::shared_ptr const &fileType : specManager->fileTypes()) {
-        if (fileType->isFolder() != isFolder) {
+        if (isReadable && fileType->isFolder() != isFolder) {
             continue;
         }
 
-        for (std::string const &pattern : fileType->filenamePatterns()) {
-            if (Wildcard::Match(pattern, fileName)) {
-                return std::make_unique<FileTypeResolver>(FileTypeResolver(filePath, fileType));
+        if (!fileType->extensions().empty()) {
+            bool matched = false;
+
+            for (std::string const &extension : fileType->extensions()) {
+                if (extension == fileExtension) {
+                    matched = true;
+                }
+            }
+
+            if (!matched) {
+                continue;
             }
         }
 
-        if (isReadable) {
+
+        if (!fileType->prefix().empty()) {
+            bool matched = false;
+
+            for (std::string const &prefix : fileType->prefix()) {
+                if (fileName.find(prefix) == 0) {
+                    matched = true;
+                }
+            }
+
+            if (!matched) {
+                continue;
+            }
+        }
+
+        if (!fileType->filenamePatterns().empty()) {
+            bool matched = false;
+
+            for (std::string const &pattern : fileType->filenamePatterns()) {
+                if (Wildcard::Match(pattern, fileName)) {
+                    matched = true;
+                }
+            }
+
+            if (!matched) {
+                continue;
+            }
+        }
+
+        if (isReadable && !fileType->permissions().empty()) {
+            bool matched = false;
+
+            std::string const &permissions = fileType->permissions();
+            if (permissions == "read") {
+                matched = isReadable;
+            } else if (permissions == "write") {
+                matched = FSUtil::TestForWrite(filePath);
+            } else if (permissions == "execute") {
+                matched = FSUtil::TestForExecute(filePath);
+            } else {
+                fprintf(stderr, "warning: unhandled permission %s\n", permissions.c_str());
+            }
+
+            if (!matched) {
+                continue;
+            }
+        }
+
+        // TODO(grp): Support TypeCodes. Not very important.
+
+        if (isReadable && !fileType->magicWords().empty()) {
+            bool matched = false;
+
             for (std::vector<uint8_t> const &magicWord : fileType->magicWords()) {
                 if (fileContents.size() < magicWord.size()) {
                     if (!fileHandle.is_open()) {
@@ -62,16 +122,19 @@ Resolve(pbxspec::Manager::shared_ptr const &specManager, std::string const &file
                 }
 
                 if (std::equal(magicWord.begin(), magicWord.end(), fileContents.begin())) {
-                    return std::make_unique<FileTypeResolver>(FileTypeResolver(filePath, fileType));
+                    matched = true;
                 }
+            }
+
+            if (!matched) {
+                continue;
             }
         }
 
-        for (std::string const &extension : fileType->extensions()) {
-            if (extension == fileExtension) {
-                return std::make_unique<FileTypeResolver>(FileTypeResolver(filePath, fileType));
-            }
-        }
+        //
+        // Matched all checks.
+        //
+        return std::make_unique<FileTypeResolver>(FileTypeResolver(filePath, fileType));
     }
 
     return nullptr;
