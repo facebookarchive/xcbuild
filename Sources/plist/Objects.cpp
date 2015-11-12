b3db3062d7032f535f877512c0cd91dc6048fb3a
Grant Paul <git@grantpaul.com>
2015-11-11 23:45:04 -0800

    Move ASCII plist parsing into a separate file, like binary and XML.

--- Sources/plist/Objects.cpp
+++ Sources/plist/Objects.cpp
@@ -10,11 +10,10 @@
 #include <plist/SimpleXMLParser.h>
 #include <plist/XMLParser.h>
 #include <plist/BinaryParser.h>
+#include <plist/ASCIIParser.h>
 
 #include <plist/Objects.h>
 #include <plist/Keys.h>
-
-#include <jsoncb.h>
 
 #include <cassert>
 #include <cstring>
@@ -380,229 +379,18 @@ dump1(FILE *fp, size_t, size_t cindent) const
 // Dictionary::Parser
 //
 
-static int _json_string(void *obj, char const *m, char const *s);
-static int _json_data(void *obj, char const *m, unsigned char const *buf, size_t len);
-static int _json_int(void *obj, char const *m, int i);
-static int _json_double(void *obj, char const *m, double d);
-static int _json_bool(void *obj, char const *m, int b);
-static int _json_null(void *obj, char const *m);
-static int _json_new_obj(void *obj, char const *m, void **obj_new, struct json_cb_t **cb);
-static int _json_obj(void *obj, void *o);
-static int _json_new_array(void *obj, char const *m, void **array_new, struct json_cb_t **cb);
-static int _json_array(void *obj, void *a);
-static void *_json_delete(void *obj);
-
-static struct json_cb_t _json_vb = {
-    _json_string,
-    _json_data,
-    _json_int,
-    _json_double,
-    _json_bool,
-    _json_null,
-
-    _json_new_obj,
-    _json_obj,
-
-    _json_new_array,
-    _json_array,
-
-    _json_delete
-};
-
-static int
-_json_string(void *obj, char const *m, char const *s)
-{
-    if (Array *a = CastTo <Array> (reinterpret_cast <Object *> (obj))) {
-        a->append(String::New(s));
-    } else if (Dictionary *d = CastTo <Dictionary> (reinterpret_cast <Object *> (obj))) {
-        assert(m != nullptr && "key is null");
-        d->set(m, String::New(s));
-    } else {
-        assert(0 && "this shouldn't happen");
-    }
-    return 0;
-}
-
-static int
-_json_data(void *obj, char const *m, unsigned char const *buf, size_t len)
-{
-    if (Array *a = CastTo <Array> (reinterpret_cast <Object *> (obj))) {
-        a->append(Data::New(buf, len));
-    } else if (Dictionary *d = CastTo <Dictionary> (reinterpret_cast <Object *> (obj))) {
-        assert(m != nullptr && "key is null");
-        d->set(m, Data::New(buf, len));
-    } else {
-        assert(0 && "this shouldn't happen");
-    }
-    return 0;
-}
-
-static int
-_json_int(void *obj, char const *m, int i)
-{
-    if (Array *a = CastTo <Array> (reinterpret_cast <Object *> (obj))) {
-        a->append(Integer::New(i));
-    } else if (Dictionary *d = CastTo <Dictionary> (reinterpret_cast <Object *> (obj))) {
-        assert(m != nullptr && "key is null");
-        d->set(m, Integer::New(i));
-    } else {
-        assert(0 && "this shouldn't happen");
-    }
-    return 0;
-}
-
-static int
-_json_double(void *obj, char const *m, double r)
-{
-    if (Array *a = CastTo <Array> (reinterpret_cast <Object *> (obj))) {
-        a->append(Real::New(r));
-    } else if (Dictionary *d = CastTo <Dictionary> (reinterpret_cast <Object *> (obj))) {
-        assert(m != nullptr && "key is null");
-        d->set(m, Real::New(r));
-    } else {
-        assert(0 && "this shouldn't happen");
-    }
-    return 0;
-}
-
-static int
-_json_bool(void *obj, char const *m, int b)
-{
-    if (Array *a = CastTo <Array> (reinterpret_cast <Object *> (obj))) {
-        a->append(Boolean::New(b != 0));
-    } else if (Dictionary *d = CastTo <Dictionary> (reinterpret_cast <Object *> (obj))) {
-        assert(m != nullptr && "key is null");
-        d->set(m, Boolean::New(b != 0));
-    } else {
-        assert(0 && "this shouldn't happen");
-    }
-    return 0;
-}
-
-static int
-_json_null(void *obj, char const *m)
-{
-    if (Array *a = CastTo <Array> (reinterpret_cast <Object *> (obj))) {
-        a->append(Null::New());
-    } else if (Dictionary *d = CastTo <Dictionary> (reinterpret_cast <Object *> (obj))) {
-        assert(m != nullptr && "key is null");
-        d->set(m, Null::New());
-    } else {
-        assert(0 && "this shouldn't happen");
-    }
-    return 0;
-}
-
-static int
-_json_new_obj(void *obj, char const *m, void **obj_new, struct json_cb_t **cb)
-{
-    Dictionary *dict = Dictionary::New();
-    if (m == nullptr) {
-        if (*reinterpret_cast <Object **> (obj) == nullptr) {
-            *reinterpret_cast <Object **> (obj) = dict;
-        } else if (Array *a = CastTo <Array> (reinterpret_cast <Object *> (obj))) {
-            a->append(dict);
-        } else {
-            assert(0 && "this shouldn't happen");
-        }
-    } else if (Dictionary *d = CastTo <Dictionary> (reinterpret_cast <Object *> (obj))) {
-        assert(m != nullptr && "key is null");
-        d->set(m, dict);
-    } else {
-        assert(0 && "this shouldn't happen");
-    }
-    *obj_new = dict;
-    *cb      = &_json_vb;
-    return 0;
-}
-
-static int
-_json_obj(void *obj, void *o)
-{
-    return 0;
-}
-
-static int
-_json_new_array(void *obj, char const *m, void **array_new, struct json_cb_t **cb)
-{
-    Array *array = Array::New();
-    if (*reinterpret_cast <Object **> (obj) == nullptr) {
-        *reinterpret_cast <Object **> (obj) = array;
-    } else if (Array *a = CastTo <Array> (reinterpret_cast <Object *> (obj))) {
-        a->append(array);
-    } else if (Dictionary *d = CastTo <Dictionary> (reinterpret_cast <Object *> (obj))) {
-        assert(m != nullptr && "key is null");
-        d->set(m, array);
-    } else {
-        assert(0 && "this shouldn't happen");
-    }
-    *array_new = array;
-    *cb        = &_json_vb;
-    return 0;
-}
-
-static int
-_json_array(void *obj, void *a)
-{
-    return 0;
-}
-
-static void *
-_json_delete(void *obj)
-{
-    return 0;
-}
-
-struct ErrorContext {
-    error_function const &ef;
-
-    ErrorContext(error_function const &func) :
-        ef(func)
-    { }
-};
-
-static int
-_json_err(void *err_data, unsigned int line, unsigned int column, char const *error)
-{
-    ErrorContext *ctx = reinterpret_cast <ErrorContext *> (err_data);
-
-    ctx->ef(line, column, error);
-
-    return -1;
-}
-
 namespace {
 
 static Object *
-JSONParse(std::string const &path, error_function const &error)
+ASCIIParse(std::FILE *fp, error_function const &error)
 {
-    Object       *root = nullptr;
-    ErrorContext  ectx(error);
-
-    if (json_parse(path.c_str(), &_json_vb, &root, _json_err, &ectx) != 0) {
-        if (root != nullptr) {
-            root->release();
-        }
-        root = nullptr;
-    }
-
-    return root;
+    return ASCIIParser().parse(fp, error);
 }
 
 static Object *
-JSONParse(std::FILE *fp, error_function const &error)
+ASCIIParse(std::string const &path, error_function const &error)
 {
-    Object       *root = nullptr;
-    ErrorContext  ectx(error);
-
-    if (json_fparse(fp, &_json_vb, &root, _json_err, &ectx) != 0) {
-        if (root != nullptr) {
-            root->release();
-        }
-        root = nullptr;
-    }
-
-    return root;
+    return ASCIIParser().parse(path, error);
 }
 
 static Object *
@@ -632,7 +420,7 @@ BinaryParse(std::FILE *fp, error_function const &error)
 enum Format {
     kFormatSystemError = -1,
     kFormatUnknown,
-    kFormatJSONOpenstep,
+    kFormatASCII,
     kFormatXML,
     kFormatBinary
 };
@@ -657,7 +445,7 @@ Identify(std::FILE *fp)
                     continue;
 
                 if (buf[n] == '(' || buf[n] == '{' || buf[n] == '/') {
-                    format = kFormatJSONOpenstep;
+                    format = kFormatASCII;
                     break;
                 } else if (buf[n] == '<') {
                     format = kFormatXML;
@@ -708,8 +496,8 @@ Parse(std::string const &path, error_function const &error)
             object = XMLParse(path, error);
             break;
 
-        case kFormatJSONOpenstep:
-            object = JSONParse(path, error);
+        case kFormatASCII:
+            object = ASCIIParse(path, error);
             break;
 
         case kFormatSystemError:
@@ -747,8 +535,8 @@ Parse(FILE *fp, error_function const &error)
             object = XMLParse(fp, error);
             break;
 
-        case kFormatJSONOpenstep:
-            object = JSONParse(fp, error);
+        case kFormatASCII:
+            object = ASCIIParse(fp, error);
             break;
 
         default:
