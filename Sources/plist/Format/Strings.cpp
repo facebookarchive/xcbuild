11d9b2c7176950d1a8bb8fb7a30d91b86ec2f0ef
Grant Paul <git@grantpaul.com>
2015-11-22 11:35:15 -0800

    Port Strings plist parsing to new Format API.
    This also removes the old Strings parser.

--- Sources/plist/Format/Strings.cpp
+++ Sources/plist/Format/Strings.cpp
@@ -8,12 +8,16 @@
  */
 
 #include <plist/Format/Strings.h>
+#include <plist/Format/ASCIIPListLexer.h>
+#include <plist/Objects.h>
 
 using plist::Format::Type;
 using plist::Format::Encoding;
 using plist::Format::Format;
 using plist::Format::Strings;
 using plist::Object;
+using plist::String;
+using plist::Dictionary;
 
 Strings::
 Strings(Encoding encoding) :
@@ -35,11 +39,141 @@ Identify(std::vector<uint8_t> const &contents)
     return nullptr;
 }
 
+/*
+ * Strings format is
+ *
+ * "key" = "value";
+ *
+ * with C-style comments.
+ */
+static bool
+StringsParserParse(ASCIIPListLexer *lexer, Dictionary *strings)
+{
+    int token;
+    int state = 0;
+    char *string;
+    String *key = NULL;
+    String *value = NULL;
+
+    for (;;) {
+        token = ASCIIPListLexerReadToken(lexer);
+        if (token < 0) {
+            if (key != NULL || value != NULL) {
+                if (key != NULL) {
+                    key->release();
+                }
+                if (value != NULL) {
+                    value->release();
+                }
+
+                return false;
+            } else if (token == kASCIIPListLexerEndOfFile) {
+                return true;
+            }
+        }
+
+        /* Ignore comments */
+        if (token == kASCIIPListLexerTokenInlineComment ||
+            token == kASCIIPListLexerTokenLongComment)
+            continue;
+
+        switch (state) {
+            case 0: /* Initial state, expect a quoted string or a keyword. */
+                if (token != kASCIIPListLexerTokenQuotedString &&
+                    token != kASCIIPListLexerTokenUnquotedString)
+                    return false;
+
+                string = ASCIIPListCopyUnquotedString(lexer, '?');
+                if (string == NULL)
+                    return false;
+
+                key = String::New(std::string(string));
+                free(string);
+                if (key == NULL)
+                    return false;
+
+                state = 1;
+                break;
+
+            case 1: /* State 1, expect a '=' */
+                if (token != kASCIIPListLexerTokenDictionaryKeyValSeparator) {
+                    key->release();
+                    return false;
+                }
+                state = 2;
+                break;
+
+            case 2: /* State 2, expect another quoted string or a keyword. */
+                if (token != kASCIIPListLexerTokenQuotedString &&
+                    token != kASCIIPListLexerTokenUnquotedString) {
+                    key->release();
+                    return false;
+                }
+
+                string = ASCIIPListCopyUnquotedString(lexer, '?');
+                if (string == NULL) {
+                    key->release();
+                    return false;
+                }
+
+                value = String::New(std::string(string));
+                free(string);
+                if (value == NULL) {
+                    key->release();
+                    return false;
+                }
+
+                state = 3;
+                break;
+
+            case 3: /* State 3, expect a semicolon */
+                if (token != ';') {
+                    value->release();
+                    key->release();
+                    return false;
+                }
+
+                strings->set(key->value(), value->copy());
+                value->release();
+                key->release();
+
+                key = NULL;
+                value = NULL;
+                state = 0; /* Back to state 0 */
+                break;
+                
+            default:
+                return false;
+        }
+    }
+}
+
 template<>
 std::pair<Object *, std::string> Format<Strings>::
 Deserialize(std::vector<uint8_t> const &contents, Strings const &format)
 {
-    return std::make_pair(nullptr, "not yet implemented");
+    Dictionary         *root;
+    std::string         error;
+    ASCIIPListLexer     lexer;
+
+    const std::vector<uint8_t> data = Encodings::Convert(contents, format.encoding(), Encoding::UTF8);
+
+    root = Dictionary::New();
+    if (root == NULL) {
+        return std::make_pair(nullptr, "unable to create root");
+    }
+
+    ASCIIPListLexerInit(&lexer, reinterpret_cast<char const *>(data.data()), data.size(), kASCIIPListLexerStyleStrings);
+
+    /* Parse contents. */
+    if (!StringsParserParse(&lexer, root)) {
+        root->release();
+        root = NULL;
+
+        error = "unable to parse strings";
+    }
+    
+    return std::make_pair(root, error);
 }
 
 template<>
