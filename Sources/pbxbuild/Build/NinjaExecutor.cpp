1d1a08b65cb911bd1524781f818a4c6fba5e62cd
Grant Paul <git@grantpaul.com>
2015-12-24 00:11:39 -0800

    Generate scheme-level Ninja file for target-level dependencies.

--- Sources/pbxbuild/Build/NinjaExecutor.cpp
+++ Sources/pbxbuild/Build/NinjaExecutor.cpp
@@ -18,6 +18,7 @@
 using pbxbuild::Build::NinjaExecutor;
 using pbxbuild::BuildEnvironment;
 using pbxbuild::BuildContext;
+using pbxbuild::TargetEnvironment;
 using pbxbuild::ToolInvocation;
 using libutil::FSUtil;
 
@@ -32,14 +33,139 @@ NinjaExecutor::
 {
 }
 
+static std::string
+TargetNinjaBegin(pbxproj::PBX::Target::shared_ptr const &target)
+{
+    return "begin-target-" + target->name();
+}
+
+static std::string
+TargetNinjaFinish(pbxproj::PBX::Target::shared_ptr const &target)
+{
+    return "finish-target-" + target->name();
+}
+
+static std::string
+TargetNinjaPath(pbxproj::PBX::Target::shared_ptr const &target, TargetEnvironment const &targetEnvironment)
+{
+    /*
+     * Determine where the Ninja file should go. We use the target's temp dir
+     * as, being target-specific, it will allow the Ninja files to not conflict.
+     */
+    pbxsetting::Environment const &environment = targetEnvironment.environment();
+    std::string temporaryDirectory = environment.resolve("TARGET_TEMP_DIR");
+    // TODO(grp): How to handle varying configurations / actions / other build context options?
+
+    return temporaryDirectory + "/" + "build.ninja";
+}
+
+static std::string
+NinjaRuleName()
+{
+    return "do";
+}
+
+static bool
+WriteNinja(ninja::Writer const &writer, std::string const &path)
+{
+    std::string contents = writer.serialize();
+
+    std::ofstream out;
+    out.open(path, std::ios::out | std::ios::trunc | std::ios::binary);
+    if (out.fail()) {
+        return false;
+    }
+
+    out.write(contents.data(), contents.size() * sizeof(char));
+    out.close();
+
+    return true;
+}
+
 bool NinjaExecutor::
 build(
     BuildEnvironment const &buildEnvironment,
     BuildContext const &buildContext,
     BuildGraph<pbxproj::PBX::Target::shared_ptr> const &targetGraph)
 {
-    bool succeeded = true;
-    for (pbxproj::PBX::Target::shared_ptr const &target : targetGraph.ordered()) {
+    /*
+     * This environment contains only settings shared for the entire build.
+     */
+    pbxsetting::Environment environment = buildEnvironment.baseEnvironment();
+    environment.insertFront(buildContext.baseSettings(), false);
+
+    /*
+     * Determine where build-level outputs will go. Note we can't use CONFIGURATION_BUILD_DIR
+     * at this point because that includes the EFFECTIVE_PLATFORM_NAME, but we don't have a platform.
+     */
+    std::string intermediatesDirectory = environment.resolve("OBJROOT");
+    // TODO(grp): How to handle varying configurations / actions / other build context options?
+
+    /*
+     * Write out a Ninja file for the build as a whole. Note each target will have a separate
+     * file, this is to coordinate the build between targets.
+     */
+    ninja::Writer writer;
+    writer.comment("xcbuild ninja");
+    // TODO(grp): Add some comments about the workspace here.
+    writer.comment("Action: " + buildContext.action());
+    writer.comment("Configuation: " + buildContext.configuration());
+    writer.newline();
+
+    /*
+     * Ninja's intermediate outputs should also go in the temp dir.
+     */
+    writer.binding({ "builddir", { ninja::Value::String(intermediatesDirectory) } });
+    writer.newline();
+
+    /*
+     * Since invocations are already resolved at this point, we can't use more specific
+     * rules at the Ninja level. Instead, add a single rule that just passes through from
+     * the build command that calls it.
+     */
+    writer.rule(NinjaRuleName(), ninja::Value::Expression("$exec"));
+
+    /*
+     * Go over each target and write out Ninja targets for the start and end of each.
+     * Don't bother topologically sorting the targets now, since Ninja will do that for us.
+     */
+    for (pbxproj::PBX::Target::shared_ptr const &target : targetGraph.nodes()) {
+
+        /*
+         * Beginning target depends on finishing the targets before that. This is implemented
+         * in three parts:
+         *
+         *  1. Each target has a "target begin" Ninja target depending on completing the build
+         *     of any dependent targets.
+         *  2. Each invocation's Ninja target depends on the "target begin" target to order
+         *     them necessarily after the target started building.
+         *  3. Each target also has a "target finish" Ninja target, which depends on all of
+         *     the invocations created for the target.
+         *
+         * The end result is that targets build in the right order. Note this does not preclude
+         * cross-target parallelization; if the target dependency graph doesn't have an edge,
+         * then they will be parallelized. Linear builds have edges from each target to all
+         * previous targets.
+         */
+
+        /*
+         * As described above, the target's begin depends on all of the target dependencies.
+         */
+        std::vector<ninja::Value> dependenciesFinished;
+        for (pbxproj::PBX::Target::shared_ptr const &dependency : targetGraph.adjacent(target)) {
+            std::string targetFinished = TargetNinjaFinish(dependency);
+            dependenciesFinished.push_back(ninja::Value::String(targetFinished));
+        }
+
+        /*
+         * Add the phony target for beginning this target's build.
+         */
+        std::string targetBegin = TargetNinjaBegin(target);
+        writer.build({ ninja::Value::String(targetBegin) }, "phony", { }, { }, dependenciesFinished);
+
+        /*
+         * Resolve this target and generate its Ninja file.
+         */
         std::unique_ptr<TargetEnvironment> targetEnvironment = buildContext.targetEnvironment(buildEnvironment, target);
         if (targetEnvironment == nullptr) {
             fprintf(stderr, "error: couldn't create target environment for %s\n", target->name().c_str());
@@ -53,7 +179,42 @@ build(
         if (!result.first) {
             return false;
         }
+
+        /*
+         * Load the Ninja file generated for this target.
+         */
+        std::string targetPath = TargetNinjaPath(target, *targetEnvironment);
+        writer.subninja(targetPath);
+
+        /*
+         * As described above, the target's finish depends on all of the invocation outputs.
+         */
+        std::vector<ninja::Value> invocationOutputs;
+        for (ToolInvocation const &invocation : phaseInvocations.invocations()) {
+            for (std::string const &output : invocation.outputs()) {
+                invocationOutputs.push_back(ninja::Value::String(output));
+            }
+        }
+
+        /*
+         * Add the phony target for ending this target's build.
+         */
+        std::string targetFinish = TargetNinjaFinish(target);
+        writer.build({ ninja::Value::String(targetFinish) }, "phony", { }, { }, invocationOutputs);
     }
+
+    /*
+     * Serialize the Ninja file into the build root.
+     */
+    std::string path = intermediatesDirectory + "/" + "build.ninja";
+    if (!WriteNinja(writer, path)) {
+        return false;
+    }
+
+    /*
+     * Note where the Ninja file is written.
+     */
+    fprintf(stderr, "Wrote meta-ninja: %s\n", path.c_str());
 
     return true;
 }
@@ -101,13 +262,7 @@ buildTarget(
 )
 {
     // TODO(grp): Handle auxiliary files and creating the directory structure.
-
-    /*
-     * Determine where the Ninja file should go. We use the target's temp dir
-     * as, being target-specific, it will allow the Ninja files to not conflict.
-     */
-    pbxsetting::Environment const &environment = targetEnvironment.environment();
-    std::string root = environment.resolve("TARGET_TEMP_DIR");
+    std::string targetBegin = TargetNinjaBegin(target);
 
     /*
      * Start building the Ninja file for this target.
@@ -116,19 +271,6 @@ buildTarget(
     writer.comment("xcbuild ninja");
     writer.comment("Target: " + target->name());
     writer.newline();
-
-    /*
-     * Ninja's intermediate outputs should also go in the temp dir.
-     */
-    writer.binding({ "builddir", { ninja::Value::String(root) } });
-
-    /*
-     * Since invocations are already resolved at this point, we can't use more specific
-     * rules at the Ninja level. Instead, add a single rule that just passes through from
-     * the build command that calls it.
-     */
-    std::string ruleName = "do";
-    writer.rule(ruleName, ninja::Value::Expression("$exec"));
 
     for (ToolInvocation const &invocation : invocations) {
         // TODO(grp): This should perhaps be a separate flag for a 'phony' invocation.
@@ -216,6 +358,11 @@ buildTarget(
         }
 
         /*
+         * All invocations depend on the target containing them beginning.
+         */
+        inputDependencies.push_back(ninja::Value::String(targetBegin));
+
+        /*
          * Build up order dependencies as literal Ninja values.
          */
         std::vector<ninja::Value> orderDependencies;
@@ -226,19 +373,16 @@ buildTarget(
         /*
          * Add the rule to build this invocation.
          */
-        writer.build(outputs, ruleName, inputs, bindings, inputDependencies, orderDependencies);
+        writer.build(outputs, NinjaRuleName(), inputs, bindings, inputDependencies, orderDependencies);
     }
 
     /*
      * Serialize the Ninja file into the build root.
      */
-    std::string contents = writer.serialize();
-    std::string path = root + "/" + "build.ninja";
-
-    std::ofstream out;
-    out.open(path, std::ios::out | std::ios::trunc | std::ios::binary);
-    out.write(contents.data(), contents.size() * sizeof(char));
-    out.close();
+    std::string path = TargetNinjaPath(target, targetEnvironment);
+    if (!WriteNinja(writer, path)) {
+        return std::make_pair(false, std::vector<ToolInvocation const>());;
+    }
 
     /*
      * Note where the Ninja file is written.
