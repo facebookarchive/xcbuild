b29cfa4bf34593c41c83a8e05f408ebbab74b23e
Grant Paul <git@grantpaul.com>
2016-01-20 22:22:12 -0800

    Add a fork of libc++'s expermental/optional as ext/optional.

---
+++ Sources/ext/optional.cpp
@@ -0,0 +1,31 @@
+/**
+ Copyright (c) 2015-present, Facebook, Inc.
+ All rights reserved.
+
+ This source code is licensed under the BSD-style license found in the
+ LICENSE file in the root directory of this source tree. An additional grant
+ of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+/*
+ * This is a modified version of libc++'s <experimental/optional> with its dependency
+ * on libc++-specific defines and includes removed to be usable with all C++11 compilers.
+ * It also moves it from namespace std to namespace ext to not conflict with the real one.
+ */
+
+//===------------------------ optional.cpp --------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is dual licensed under the MIT and the University of Illinois Open
+// Source Licenses. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include <ext/optional>
+
+namespace ext {
+
+bad_optional_access::~bad_optional_access() _NOEXCEPT = default;
+
+}
