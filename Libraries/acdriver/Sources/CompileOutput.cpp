d690825c40d955dd33ea968bcaebe7c15c90daae
Grant Paul <github@grantpaul.com>
2016-07-03 14:27:32 -0700

    Add asset catalog driver compile action output.
    Includes a basic unit test.

---
+++ Libraries/acdriver/Sources/CompileOutput.cpp
@@ -0,0 +1,92 @@
+/**
+ Copyright (c) 2015-present, Facebook, Inc.
+ All rights reserved.
+
+ This source code is licensed under the BSD-style license found in the
+ LICENSE file in the root directory of this source tree. An additional grant
+ of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#include <acdriver/CompileOutput.h>
+#include <acdriver/Options.h>
+#include <acdriver/Result.h>
+#include <libutil/Filesystem.h>
+#include <plist/Format/Format.h>
+#include <plist/Format/XML.h>
+
+using acdriver::CompileOutput;
+using acdriver::Options;
+using acdriver::Result;
+using libutil::Filesystem;
+
+CompileOutput::
+CompileOutput(std::string const &root, Format format) :
+    _root          (root),
+    _format        (format),
+    _additionalInfo(plist::Dictionary::New())
+{
+}
+
+bool CompileOutput::
+write(Filesystem *filesystem, ext::optional<std::string> const &partialInfoPlist, ext::optional<std::string> const &dependencyInfo, Result *result) const
+{
+    bool success = true;
+
+    /*
+     * Write out compiled archive.
+     */
+    if (_car) {
+        // TODO: only write if non-empty. but did mmap already create the file?
+        _car->write();
+    }
+
+    /*
+     * Copy files into output.
+     */
+    for (std::pair<std::string, std::string> const &copy : _copies) {
+        std::vector<uint8_t> contents;
+
+        if (!filesystem->read(&contents, copy.first)) {
+            result->normal(Result::Severity::Error, "unable to read input: " + copy.first);
+            success = false;
+            continue;
+        }
+
+        if (!filesystem->write(contents, copy.second)) {
+            result->normal(Result::Severity::Error, "unable to write output: " + copy.second);
+            success = false;
+            continue;
+        }
+    }
+
+    /*
+     * Write out partial info plist, if requested.
+     */
+    if (partialInfoPlist) {
+        auto format = plist::Format::XML::Create(plist::Format::Encoding::UTF8);
+        auto serialize = plist::Format::XML::Serialize(_additionalInfo.get(), format);
+        if (serialize.first == nullptr) {
+            result->normal(Result::Severity::Error, "unable to serialize partial info plist");
+            success = false;
+        } else {
+            if (!filesystem->write(*serialize.first, *partialInfoPlist)) {
+                result->normal(Result::Severity::Error, "unable to write partial info plist");
+                success = false;
+            }
+        }
+    }
+
+    /*
+     * Write out dependency info, if requested.
+     */
+    if (dependencyInfo) {
+        if (!filesystem->write(_dependencyInfo.serialize(), *dependencyInfo)) {
+            result->normal(Result::Severity::Error, "unable to write dependency info");
+            success = false;
+        }
+    }
+
+    return success;
+}
+
+
---
+++ Libraries/acdriver/Tests/test_CompileOutput.cpp
@@ -0,0 +1,57 @@
+/**
+ Copyright (c) 2015-present, Facebook, Inc.
+ All rights reserved.
+
+ This source code is licensed under the BSD-style license found in the
+ LICENSE file in the root directory of this source tree. An additional grant
+ of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#include <gtest/gtest.h>
+#include <acdriver/CompileOutput.h>
+#include <acdriver/Result.h>
+#include <libutil/MemoryFilesystem.h>
+
+using acdriver::CompileOutput;
+using acdriver::Result;
+using libutil::MemoryFilesystem;
+
+static std::vector<uint8_t>
+Contents(std::string const &string)
+{
+    return std::vector<uint8_t>(string.begin(), string.end());
+}
+
+TEST(CompileOutput, Empty)
+{
+    MemoryFilesystem filesystem = MemoryFilesystem({ });
+    CompileOutput output = CompileOutput("/", CompileOutput::Format::Compiled);
+
+    Result result;
+    EXPECT_TRUE(output.write(&filesystem, ext::nullopt, ext::nullopt, &result));
+    EXPECT_TRUE(result.success());
+}
+
+TEST(CompileOutput, Copy)
+{
+    MemoryFilesystem filesystem = MemoryFilesystem({
+        MemoryFilesystem::Entry::File("source1", Contents("one")),
+        MemoryFilesystem::Entry::File("source2", Contents("two")),
+    });
+
+    /* Copy files from input to output. */
+    CompileOutput output = CompileOutput("/", CompileOutput::Format::Compiled);
+    output.copies().push_back({ "/source1", "/dest1" });
+    output.copies().push_back({ "/source2", "/dest2" });
+
+    Result result;
+    EXPECT_TRUE(output.write(&filesystem, ext::nullopt, ext::nullopt, &result));
+    EXPECT_TRUE(result.success());
+
+    /* Contents should have been copied. */
+    std::vector<uint8_t> contents;
+    ASSERT_TRUE(filesystem.read(&contents, "/dest1"));
+    EXPECT_EQ(contents, Contents("one"));
+    ASSERT_TRUE(filesystem.read(&contents, "/dest2"));
+    EXPECT_EQ(contents, Contents("two"));
+}
