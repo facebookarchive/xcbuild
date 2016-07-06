71b3f4e5a1788850dc10f1186d6285926794458e
Matthew Glazar <strager@fb.com>
2016-07-06 11:18:48 -0700

    Prefix installation path with "Developer/".
    Instead of installing executables into
    ${CMAKE_INSTALL_PREFIX}/usr/bin/ (e.g. /usr/local/usr/bin/),
    install them into
    ${CMAKE_INSTALL_PREFIX}/${XCBUILD_INSTALL_DEVELOPER_DIR}/usr/bin/
    (e.g. /usr/local/Developer/usr/bin/).
    (${XCBUILD_INSTALL_DEVELOPER_DIR} defaults to "Developer".)
    
    In the future, we will install some executables into
    ${CMAKE_INSTALL_PREFIX}/bin/ (e.g. /usr/local/bin/).
    This commit clearly separates Developer-installed files from
    $PATH-installed files.
    
    To get the old behaviour, run CMake with
    "-DXCBUILD_INSTALL_DEVELOPER_DIR:STRING=." (or run "make
    edit_cache"). If you are using xcbuild's Makefile, run "make
    install PREFIX=/opt" instead of "make install
    PREFIX=/opt/Developer".

--- CMake/Install.cmake
+++ CMake/Install.cmake
@@ -7,9 +7,11 @@
 # of patent rights can be found in the PATENTS file in the same directory.
 #
 
+set(XCBUILD_INSTALL_DEVELOPER_DIR Developer CACHE STRING "Install path for xcbuild support files")
+
 # XCBUILD_INSTALL_TARGET(foo bar qix) installs the CMake targets 'foo', 'bar',
 # and 'qix' into the xcbuild Developer dir (specified by
-# CMAKE_INSTALL_PREFIX).
+# XCBUILD_INSTALL_DEVELOPER_DIR).
 function (XCBUILD_INSTALL_TARGET)
   cmake_parse_arguments("" "" "" "" ${ARGN})
   set(TARGETS ${_UNPARSED_ARGUMENTS})
@@ -17,6 +19,6 @@ function (XCBUILD_INSTALL_TARGET)
   # libraries (including Windows import libraries).
   # TODO(strager): Handle FRAMEWORK and BUNDLE targets.
   install(TARGETS ${TARGETS}
-          RUNTIME DESTINATION usr/bin
-          LIBRARY DESTINATION usr/lib)
+          RUNTIME DESTINATION ${XCBUILD_INSTALL_DEVELOPER_DIR}/usr/bin
+          LIBRARY DESTINATION ${XCBUILD_INSTALL_DEVELOPER_DIR}/usr/lib)
 endfunction ()
