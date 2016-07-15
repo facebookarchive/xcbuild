1242d7e71162ba2aa2d88e1b881d770808a7de18
Grant Paul <git@grantpaul.com>
2016-07-15 01:20:02 -0700

    Support both ImageIO and libpng for reading PNG images.
     - Separate out PNG loading into a separate module.
     - Refactor PNG loading to be less tied to asset compliation.
     - Add an implementation of PNG loading using ImageIO when available.
     - Add a unit test for loading various types of PNG images.

---
+++ Libraries/graphics/Headers/graphics/Format/PNG.h
@@ -0,0 +1,43 @@
+/**
+ Copyright (c) 2015-present, Facebook, Inc.
+ All rights reserved.
+
+ This source code is licensed under the BSD-style license found in the
+ LICENSE file in the root directory of this source tree. An additional grant
+ of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#ifndef __graphics_Format_PNG_h
+#define __graphics_Format_PNG_h
+
+#include <graphics/Image.h>
+#include <graphics/PixelFormat.h>
+
+#include <string>
+#include <utility>
+#include <vector>
+#include <ext/optional>
+
+namespace graphics {
+namespace Format {
+
+/*
+ * Utilities for PNG images.
+ */
+class PNG {
+private:
+    PNG();
+    ~PNG();
+
+public:
+    /*
+     * Read a PNG image.
+     */
+    static std::pair<ext::optional<Image>, std::string>
+    Read(std::vector<uint8_t> const &contents);
+};
+
+}
+}
+
+#endif // !__graphics_Format_PNG_h
