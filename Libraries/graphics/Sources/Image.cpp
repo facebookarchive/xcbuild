2a87c46cc618fad5ed08a5dc0c10d08d9e4deaac
Grant Paul <git@grantpaul.com>
2016-07-17 15:29:40 -0700

    Add representation of an image.

---
+++ Libraries/graphics/Sources/Image.cpp
@@ -0,0 +1,24 @@
+/**
+ Copyright (c) 2015-present, Facebook, Inc.
+ All rights reserved.
+
+ This source code is licensed under the BSD-style license found in the
+ LICENSE file in the root directory of this source tree. An additional grant
+ of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#include <graphics/Image.h>
+#include <cassert>
+
+using graphics::Image;
+
+Image::
+Image(size_t width, size_t height, PixelFormat format, std::vector<uint8_t> const &data) :
+    _width (width),
+    _height(height),
+    _format(format),
+    _data  (data)
+{
+    assert(data.size() == _width * _height * _format.bytesPerPixel());
+}
+
